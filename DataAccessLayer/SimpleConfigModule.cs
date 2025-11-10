aabc21d0f54ed0e289aae3f05842cbcfe1a5343e
t3mm1k <t3mm1k@gmail.com>
2025-11-10 12:02:54 +0700

    Add Ninject configuration module for dependency injection

---
+++ DataAccessLayer/SimpleConfigModule.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DataAccessLayer
+{
+    public class SimpleConfigModule : Ninject.Modules.NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IRepository<Student>>().To<EntityRepository>().InSingletonScope();
+        }
+    }
+}
