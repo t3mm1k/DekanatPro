54205c1683990ab0ffac75758b19c82d58bc3c8b
Dmitry NoteBook <you@example.com>
2025-11-17 15:03:53 +0700

    feat: Add StudentEventArgs

--- Shared/StudentEventArgs.cs
+++ Shared/StudentEventArgs.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Shared
 {
-    public class Class1
+    public class StudentEventArgs : EventArgs
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Speciality { get; set; }
+        public string Group { get; set; }   
     }
 }
