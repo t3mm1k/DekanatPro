b593e76cb852320e9ea9e6cb47242cbaa851cbe8
t3mm1k <t3mm1k@gmail.com>
2025-11-17 15:18:48 +0700

    feat: Add Delete Insert methods to Student BL

--- WindowsForm View/Form1.Designer.cs
+++ WindowsForm View/Form1.Designer.cs
@@ -28,9 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.lblName = new System.Windows.Forms.Label();
             this.lblSpeciality = new System.Windows.Forms.Label();
             this.lblGroup = new System.Windows.Forms.Label();
@@ -39,11 +36,8 @@
             this.txtGroup = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnDelete = new System.Windows.Forms.Button();
-            this.btnShowHistogram = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // lblName
@@ -112,15 +106,6 @@
             this.btnDelete.Text = "Удалить";
             this.btnDelete.UseVisualStyleBackColor = true;
             // 
-            // btnShowHistogram
-            // 
-            this.btnShowHistogram.Location = new System.Drawing.Point(242, 270);
-            this.btnShowHistogram.Name = "btnShowHistogram";
-            this.btnShowHistogram.Size = new System.Drawing.Size(127, 23);
-            this.btnShowHistogram.TabIndex = 10;
-            this.btnShowHistogram.Text = "Гистограмма";
-            this.btnShowHistogram.UseVisualStyleBackColor = true;
-            // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
@@ -131,30 +116,12 @@
             this.dataGridView1.Size = new System.Drawing.Size(706, 298);
             this.dataGridView1.TabIndex = 11;
             // 
-            // chart1
-            // 
-            chartArea5.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea5);
-            legend5.Name = "Legend1";
-            this.chart1.Legends.Add(legend5);
-            this.chart1.Location = new System.Drawing.Point(34, 328);
-            this.chart1.Name = "chart1";
-            series5.ChartArea = "ChartArea1";
-            series5.Legend = "Legend1";
-            series5.Name = "Series1";
-            this.chart1.Series.Add(series5);
-            this.chart1.Size = new System.Drawing.Size(1077, 300);
-            this.chart1.TabIndex = 12;
-            this.chart1.Text = "chart1";
-            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1116, 665);
-            this.Controls.Add(this.chart1);
+            this.ClientSize = new System.Drawing.Size(1108, 319);
             this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.btnShowHistogram);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtGroup);
@@ -165,9 +132,7 @@
             this.Controls.Add(this.lblName);
             this.Name = "Form2";
             this.Text = "Form1";
-            //this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -183,9 +148,7 @@
         private System.Windows.Forms.TextBox txtGroup;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.Button btnShowHistogram;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
