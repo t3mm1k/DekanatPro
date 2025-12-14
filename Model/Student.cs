3b3c6122cf07ebe6596650face1fc35b76ae637c
Dmitry NoteBook <you@example.com>
2025-12-15 00:43:54 +0700

    feat: add Model with INotifyPropertyChanged

---
+++ Model/Student.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Student : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        private string _name;
+
+        public string Group
+        {
+            get
+            {
+                return _group;
+            }
+            set
+            {
+                _group = value;
+                OnPropertyChanged(nameof(Group));
+            }
+        }
+        private string _group;
+
+        public string Speciality
+        {
+            get
+            {
+                return _speciality;
+            }
+            set
+            {
+                _speciality = value;
+                OnPropertyChanged(nameof(Speciality));
+            }
+        }
+        private string _speciality;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
